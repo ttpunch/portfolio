be480ebafb286847efb2d2e259309e440d25fffa
ttpunch <vinod418@gmail.com>
2024-08-27 22:17:08 +0530

    second commit

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,8 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-
+import Image from "next/image";
+import CheckCircle from "../assets/icons/check-circle.svg";
 const portfolioProjects = [
   {
     company: "Acme Corp",
@@ -42,31 +43,50 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>
-    <div className="container">
-      <p>Real World Results</p>
-      <h2>Featured Projects</h2>
-      <p>See how i transformed concept into engaging digital experiences</p>
-      <div>
-        {portfolioProjects.map((project, index) => (
-          <div key={index}>
-            <div>
-              <span>{project.company}</span>
-              <span>{project.year}</span>
-
+  return (
+    <div>
+      <div className="container">
+        <p className=" text-center text-semibold  uppercase tracking-widest bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-transparent">
+          REAL-WORLD RESULTS
+        </p>
+        <h2 className="text-center font-serif text-3xl mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/40 mt-4">
+          See how i transformed concept into engaging digital experiences
+        </p>
+        <div>
+          {portfolioProjects.map((project, index) => (
+            <div
+              key={index}
+              className="bg-gray-800 flex flex-col lg:flex-row overflow-hidden justify-start w-[98%] h-auto border border-white/40 rounded-3xl mx-auto px-8 py-8 mt-6"
+            >
+              <div className="lg:flex lg:flex-col lg:w-full lg:gap-4 lg:px-10 lg:py-10">
+              <div className="text-semibold  uppercase tracking-widest bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-transparent">
+                <span>{project.company} . </span>
+                <span>{project.year}</span>
+              </div>
+              <h3 className="text-2xl lg:text-3xl font-serif mt-2 mb-4">{project.title}</h3>
+              <hr className="opacity-20" />
+              <ul className="flex flex-col space-y-4 mt-4 text-white/50">
+                {project.results.map((result) => (
+                  <li className="flex gap-2 text-sm " key={result.title}>
+                    <CheckCircle className="size-5" />
+                    {result.title}
+                  </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="rounded-xl bg-white text-gray-800 px-4 py-2 w-full mt-6 mb-10 lg:mb-4">
+                  View live status
+                </button>
+              </a>
+              </div>
+              <Image src={project.image} alt="Project image" className="lg:size-1/2 lg:mt-6"/>
             </div>
-            <h3>{project.title}</h3>
-            <hr/>
-            <ul>
-              {project.results.map((result)=>(
-              <li key={result.title}>{result.title}</li>
-              ))}
-            </ul>
-
-          </div>)
-        )}
+          ))}
+        </div>
       </div>
-
     </div>
-  </div>;
+  );
 };
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,32 @@
+import StartIcon from "../assets/icons/star.svg";
+const portfolioKeywords = [
+  "Creativity",
+  "Innovation",
+  "Expertise",
+  "Design",
+  "Solutions",
+  "Passion",
+  "Quality",
+  "Professionalism",
+  "Craftsmanship",
+  "Vision",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 ">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-300 text-clip text-gray-800 overflow-x-clip">
+      <div className="flex gap-2 py-3 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      {portfolioKeywords.map((result) => {
+        return (
+          <div key={result} className="inline-flex gap-2">
+            <span  className="text-gray-900 uppercase font-extrabold">{result}</span>
+            <StartIcon />
+          </div>
+        );
+      })}
+      </div>
+      </div>
+    </div>
+  );
 };
