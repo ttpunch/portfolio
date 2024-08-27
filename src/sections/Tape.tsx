2f11748e42cf4f9b7a70daa13a440e9bead5101b
ttpunch <vinod418@gmail.com>
2024-08-27 23:08:36 +0530

    adding framer and react-fast-marquee npm package

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,6 @@
+"use client"
+import {motion} from "framer-motion"
+import Marquee from "react-fast-marquee";
 import StartIcon from "../assets/icons/star.svg";
 const portfolioKeywords = [
   "Creativity",
@@ -14,9 +17,10 @@ const portfolioKeywords = [
 
 export const TapeSection = () => {
   return (
-    <div className="py-16 ">
-      <div className="bg-gradient-to-r from-emerald-300 to-sky-300 text-clip text-gray-800 overflow-x-clip">
-      <div className="flex gap-2 py-3 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+    <div className="py-16  overflow-x-clip container ">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-300 text-clip text-gray-800 overflow-x-clip rounded-md">
+      <div className="flex gap-2 py-3 [mask-image:linear-gradient(to_right,transparent,black_15%,black_85%,transparent)]">
+      <Marquee pauseOnHover={true} speed={30} autoFill={true}>
       {portfolioKeywords.map((result) => {
         return (
           <div key={result} className="inline-flex gap-2">
@@ -25,6 +29,7 @@ export const TapeSection = () => {
           </div>
         );
       })}
+      </Marquee>
       </div>
       </div>
     </div>
